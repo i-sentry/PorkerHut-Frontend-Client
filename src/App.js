7f71b8c06fa22507abbfa3f793a81e38352dc785
Wild-dev <widorenyin0@gmail.com>
2023-01-06 23:15:13 +0000

    setting up more dep

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-
-
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <h1>hello Porker</h1>
+    <div className="App flex items-center justify-center h-screen bg-slate-500">
+      <h1 className="text-xl text-[#0bd9]">hello Porker</h1>
     </div>
   );
 }
---
+++ src/services/api.ts
@@ -0,0 +1,74 @@
+import axios from "axios";
+
+export const BASEURL = process.env.REACT_APP_BASE_URL || "";
+
+export const api = {};
+
+export const makePostRequest = async (
+  data: any,
+  url: string,
+  includeAuthHeader: boolean = true
+) => {
+  return await axios.post(`${BASEURL}${url}`, data, {
+    headers: {
+      "x-access-token": localStorage.getItem("accessToken") as string,
+    },
+  });
+};
+
+export const makeDeleteRequest = async <T = any>(
+  url: string,
+  includeAuthHeader: boolean = true
+) => {
+  return await axios.delete<T>(`${BASEURL}${url}`, {
+    headers: {
+      "x-access-token": localStorage.getItem("accessToken") as string,
+    },
+  });
+};
+
+export const makePostRequestWithCustomHeaders = async (
+  data: any,
+  url: string,
+  headers?: any
+) => {
+  const temp = await fetch(`${BASEURL}${url}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!temp.ok) {
+    throw new Error(await temp.text());
+  }
+
+  const result = await temp.json();
+  return result;
+};
+
+export const makePatchRequest = async (
+  data: any,
+  url: string,
+  includeAuthHeader: boolean = true
+) => {
+  return await axios.patch(`${BASEURL}${url}`, data, {
+    headers: {
+      "x-access-token": localStorage.getItem("accessToken") as string,
+    },
+  });
+};
+
+export const makeGetRequest = async <T = any>(
+  url: string,
+  includeAuthHeaders: boolean = true
+) => {
+  const temp = await axios.get<T>(`${BASEURL}${url}`, {
+    headers: {
+      "x-access-token": localStorage.getItem("accessToken") as string,
+    },
+  });
+  return temp;
+};
