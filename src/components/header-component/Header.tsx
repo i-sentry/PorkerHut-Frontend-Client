eab21f775aeef5e1b6492c7302a699f2c49ef6cb
tonyekim <otoobongekim@gmail.com>
2023-02-01 09:20:30 +0100

    change a text in the header component

--- src/components/header-component/Header.tsx
+++ src/components/header-component/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <div className="flex justify-center items-center md:mx-10 xxs:mt-4">
-        <h1 className="font-semibold text-2xl">Shop by Category</h1>
+        <h1 className="font-semibold text-2xl">Featured Products</h1>
       </div>
       <div className="flex items-center justify-center mb-10">
         <div className=" block h-1 w-20 bg-[#197B30]"></div>
