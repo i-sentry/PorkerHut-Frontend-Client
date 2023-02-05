78ea93d83644c9c18deb9140665a3978bac32134
tonyekim <otoobongekim@gmail.com>
2023-02-05 07:22:28 +0100

    Updating file

--- src/components/blog-banner-component/BlogCard.tsx
+++ src/components/blog-banner-component/BlogCard.tsx
@@ -5,7 +5,7 @@ let blog_img2 = require("../../assets/images/Blog_frame-2.jpg");
 
 const Blog = () => {
   return (
-    <section className="w-full md:h-[500px] xxs:h-[950px]">
+    <section className="w-full md:h-[500px] xxs:h-[900px]">
       <div className="w-full max-h-[1000px] md:grid md:grid-cols-2 md:px-10  xxs:grid-cols-1 xxs:px-3 ">
         <div className="flex flex-col items-center w-full xxs:mb-10">
           <div className="md:w-[80%]">
@@ -16,7 +16,7 @@ const Blog = () => {
             />
           </div>
 
-          <div className="bottom md:w-[70%] bg-white md:h-[200px] px-8 mt-[-100px] md:border-2 xxs:border md:pt-4 xxs:w-[80%] xxs:h-[300px] xxs:pt-6">
+          <div className="bottom md:w-[70%] bg-white md:h-[200px] px-8 mt-[-100px] md:border-2 xxs:border md:pt-4 xxs:w-[90%] xxs:h-[260px] xxs:pt-6">
             <div className="md:flex md:items-center md:justify-between">
               <h1 className=" font-semibold">
                 The monetary aspect of Pig farm
@@ -45,7 +45,7 @@ const Blog = () => {
             />
           </div>
 
-          <div className="bottom md:w-[70%] bg-white md:h-[200px] px-8 mt-[-100px] md:border-2 xxs:border md:pt-4 xxs:w-[80%] xxs:h-[300px] xxs:pt-6">
+          <div className="bottom md:w-[70%] bg-white md:h-[200px] px-8 mt-[-100px] md:border-2 xxs:border md:pt-4 xxs:w-[90%] xxs:h-[260px] xxs:pt-6">
             <div className="md:flex md:items-center md:justify-between">
               <h1 className="font-semibold">The monetary aspect of Pig farm</h1>
               <span className="text-xs text-[#B4B4B4]">Jan 20, 2022</span>
