97cbe24450194bc23cb5c9965c94651e3c3aa8c3
Emmanuel Nwakanma <emmynetro@gmail.com>
2023-02-04 22:22:25 -0500

    about page for PR

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,7 +14,7 @@ const About = () => {
       
         <div className=" relative ">
           <img className=" opacity-150 relative brightness-50 mt-20" src={Pigdesktop} alt="" />
-          <div className=" absolute px-[8%] top-1/2 -translate-y-1/3 mx-24">
+          <div className=" absolute px-[4%] top-1/2 -translate-y-1/3 mx-12">
             <h1 className=" text-[#FFFF] text-2xl md:text-[40px] md:py-5">About Us</h1>
             <BreadCrumb
               items={[
@@ -35,8 +35,8 @@ const About = () => {
 
 
 
-        <div className=" bg-[#F4F4F4] mt-24 rounded-md max-w-[1700px] m-auto md:flex">
-          <div className=" p-20 flex-1">
+        <div className=" bg-[#F4F4F4] mt-24 rounded-md max-w-[1360px] m-auto md:flex">
+          <div className=" p-16 flex-1">
             <h1 className=" text-4xl mb-4 font-bold text-[#333]">What we do</h1>
             <p className=" leading-6 text-[14px] md:text-[16px] text-[#333]">Lorem ipsum dolor sit amet consectetur. Tristique rhoncus nulla    mollis dui in in in aliquet. 
               Dapibus aenean sed felis adipiscing habitant a amet volutpat. Auctor donec at nisl justo
@@ -55,7 +55,7 @@ const About = () => {
         </div>
 
         <div className=" mt-24 p-4 bg-[#F4F4F4] flex-row">
-      <div className=" md:ml-44 bg-[#F4F4F4]">
+      <div className=" md:ml-24 bg-[#F4F4F4]">
               <div className="max-w-[500px]">
               <h1 className=" text-[28px] mb-4 font-bold text-[#333]">Why choose Pokerhut</h1>
                 <p className=" text-[#333] leading-6 text-[14px] md:text-[16px]">Lorem ipsum dolor sit amet consectetur. Tristique rhoncus nulla    mollis dui in in in aliquet. 
@@ -65,8 +65,8 @@ const About = () => {
                 </p>
               </div>
 
-          <div className=" md:flex gap-16  ">
-              <div className="max-w-[500px] bg-white rounded mt-12 px-2 py-2">
+          <div className=" md:flex gap-4 mb-10 ">
+              <div className="max-w-[450px] bg-white rounded mt-12 px-2 py-2 ">
               <h1 className=" text-2xl mb-4 font-bold text-[#333]">Personalized Products</h1>
                 <p className=" text-[#333] leading-6 text-[14px] md:text-[16px]">Lorem ipsum dolor sit amet consectetur. Tristique rhoncus nulla    mollis dui in in in aliquet. 
                     Dapibus aenean sed felis adipiscing habitant a amet volutpat. Auctor donec at nisl justo
@@ -75,7 +75,7 @@ const About = () => {
               </p>
               </div>
 
-              <div className="max-w-[500px] bg-white rounded mt-12 px-2 py-2">
+              <div className="max-w-[450px] bg-white rounded mt-12 px-2 py-2">
               <h1 className=" text-2xl mb-4 font-bold text-[#333]">Availabilty</h1>
                 <p className=" text-[#333] leading-6 text-[14px] md:text-[16px]">Lorem ipsum dolor sit amet consectetur. Tristique rhoncus nulla    mollis dui in in in aliquet. 
                     Dapibus aenean sed felis adipiscing habitant a amet volutpat. Auctor donec at nisl justo
@@ -84,7 +84,7 @@ const About = () => {
               </p>
               </div>
               
-              <div className="max-w-[500px] bg-white rounded mt-12 px-2 py-2">
+              <div className="max-w-[450px] bg-white rounded mt-12 px-2 py-2">
               <h1 className=" text-2xl mb-4 font-bold text-[#333]">Guaranteed Satisfaction</h1>
                 <p className=" leading-6 text-[14px] md:text-[16px] text-[#333]">Lorem ipsum dolor sit amet consectetur. Tristique rhoncus nulla    mollis dui in in in aliquet. 
                     Dapibus aenean sed felis adipiscing habitant a amet volutpat. Auctor donec at nisl justo
