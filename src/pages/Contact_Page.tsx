72f7be0e3eb66b30ed173626e745bcd375ba472e
Wild-dev <widorenyin0@gmail.com>
2023-02-12 15:55:06 +0000

    Resolve fixes

--- src/pages/Contact_Page.tsx
+++ src/pages/Contact_Page.tsx
@@ -7,48 +7,48 @@ import Footer from "../components/footer-component/Footer";
 
 const Contact_Page = () => {
   return (
-    <div className="overflow-x-hidden">
-      <nav className="mb-20">
-        <NavBar />
-      </nav>
-      <div className="container  max-w-screen-xl ">
-        <div
-          className=" md:block xxs:flex flex-col items-center justify-center w-full md:h-[330px] bg-cover bg-center py-20 px-14"
-          style={{
-            backgroundImage: ` url('${headerImg}')`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            width: "100vw",
-          }}
-        >
-          <h1 className="mb-2 text-3xl font-medium  text-left   text-[#fff]">
-            Contact Us
-          </h1>
+      <div className="overflow-x-hidden">
+        <nav className="mb-20">
+          <NavBar />
+        </nav>
+        <div className="container  max-w-screen-xl ">
+          <div
+            className=" md:block xxs:flex flex-col items-center justify-center w-full md:h-[330px] bg-cover bg-center py-20 px-14"
+            style={{
+              backgroundImage: ` url('${headerImg}')`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              width: "100vw",
+            }}
+          >
+            <h1 className="mb-2 text-3xl font-medium  text-left   text-[#fff]">
+              Contact Us
+            </h1>
 
-          <BreadCrumbs
-            items={[
-              {
-                name: "Home",
-                link: "/",
-              },
-              {
-                name: "Contact Us",
-                link: "/contact-us",
-              },
-            ]}
-          />
+            <BreadCrumbs
+              items={[
+                {
+                  name: "Home",
+                  link: "/",
+                },
+                {
+                  name: "Contact Us",
+                  link: "/contact-us",
+                },
+              ]}
+            />
+          </div>
         </div>
+        <section className="w-full h-full md:p-14">
+          <ServiceForm
+            text="How can we help? Please contact us and we will get back to you as soon as possible. If you have an inquiry about your order, you can respond directly to the order confirmation email or contact us via chat."
+            title="Send us a Message"
+          />
+        </section>
+        <footer>
+          <Footer />
+        </footer>
       </div>
-      <section className="w-full h-full md:p-14">
-        <ServiceForm
-          text="How can we help? Please contact us and we will get back to you as soon as possible. If you have an inquiry about your order, you can respond directly to the order confirmation email or contact us via chat."
-          title="Send us a Message"
-        />
-      </section>
-      <footer>
-        <Footer />
-      </footer>
-    </div>
   );
 };
 
