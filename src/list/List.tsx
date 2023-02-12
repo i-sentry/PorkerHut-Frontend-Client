b8d48bda75cd698b7b30dbf694a943488306f28e
tonyekim <otoobongekim@gmail.com>
2023-02-12 13:02:44 +0100

    Product page filter modal modification

--- src/list/List.tsx
+++ src/list/List.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ProductCard from "../components/featured-product-component/ProductCard";
-import { productData } from "../utils/productData";
 
-const List = () => {
+
+const List = ({Data}: any) => {
   return (
     <div className="grid md:grid-cols-4 mb-6 xxs:grid-cols-2">
-      {productData?.map((item) => (
+      {Data?.map((item: any) => (
         <ProductCard item={item} key={item.id} />
       ))}
     </div>
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,5 +1,5 @@
 import { log } from "console";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 import List from "../list/List";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
@@ -9,11 +9,39 @@ import NavBar from "../components/nav-component/NavBar";
 
 import Footer from "../components/footer-component/Footer";
 import ProductsBreadCrumbs from "../components/story-components/ProductsBreadCrumbs";
+import { MdOutlineFilterAlt } from "react-icons/md";
+import { productData } from "../utils/productData";
+import FilterSidebar from "../components/accordion-component/FilterSidebarModal";
 
-const ProductPage = ({ item }: any) => {
+const ProductPage = ({}) => {
+
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const [post, setPost] = useState<any[]>([])
+  const [number, setNumber] = useState(1)
+  const postPerPage = 20
+  
+  const lastPost = number * postPerPage;
+  const firstPost = lastPost - postPerPage
+  const currentPost = post.slice(firstPost, lastPost)
+  const [data, setData] = useState(productData);
+  
+  const pageNumber = []
+  for (let i = 1; 1 <= Math.ceil(post.length / postPerPage); i++) {
+    pageNumber.push(i)
+  }
+
+
+  
+
+  console.log(pageNumber, "pageNumber");
+  //@ts-ignore
+  const menuItems = [...new Set(productData.map((d: any) => d.category))];
+
   return (
-    <div className="bg-[#EEEEEE]">
-      <NavBar />
+    <div className="bg-[#EEEEEE] overflow-hidden relative">
+      <NavBar/>
+      <FilterSidebar open={openModal} onClose={() => setOpenModal(false)} />
       <div className="bg-[#EEEEEE] pt-24">
         <div className="px-8">
           <ProductsBreadCrumbs
@@ -31,15 +59,14 @@ const ProductPage = ({ item }: any) => {
         </div>
 
         <div className="md:flex">
-          <div className="md:w-1/4 static h-full top-[50px] bg-white p-6 mx-6 xxs:hidden md:block ">
-            <Filter />
+          <div className="md:w-1/4 static h-full top-[50px] bg-white p-6 mx-6 xxs:hidden md:block overflow-hidden">
+            <Filter setData={setData} menuItem={menuItems} />
           </div>
           <div className="md:w-3/4 bg-white xxs:w-full">
             <div className="flex items-center justify-between  pl-3">
-              <div className="flex items-center justify-between gap-16">
-
-              <h1 className="text-xl font-medium">All Products</h1>
-               <div>
+              <div className="md:flex md:items-center md:justify-between md:gap-16 xxs:py-4">
+                <h1 className="text-xl font-medium">All Products</h1>
+                <div>
                   <p className="text-l text-gray-700">
                     Showing <span className="font-medium">1</span> -{" "}
                     <span className="font-medium">10</span> of{" "}
@@ -47,86 +74,54 @@ const ProductPage = ({ item }: any) => {
                   </p>
                 </div>
               </div>
-               
+
               <div className="flex items-center ">
-                <span className="pt-2 ml-14 text-base font-normal text-[#BDBDBD]">
+                <span className="pt-2 ml-14 text-base font-normal text-[#BDBDBD] xxs:hidden md:block">
                   Sort by:
                 </span>
-                <span>
+                <span className="xxs:hidden md:block">
                   <Sort />
                 </span>
+                <div className="md:hidden xxs:block flex justify-center items-end gap-2 px-2">
+                  <MdOutlineFilterAlt className="inline" size={22} onClick={() => setOpenModal(true)} />
+                  <span className="text-sm">Filter Products</span>
+                </div>
               </div>
             </div>
             <hr className="mx-3 bg-[#D9D9D9] border my-2" />
 
-            <List />
+            <List Data={data} />
             <div className="flex items-center justify-center border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-              
               <div className="">
-                
                 <div className="">
                   <nav
                     className="isolate inline-flex -space-x-px rounded-md shadow-sm"
                     aria-label="Pagination"
                   >
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
-                    >
+                    <button className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
                       <span className="sr-only">Previous</span>
                       <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-                    </a>
-              
-                    <a
-                      href="#"
-                      aria-current="page"
-                      className="relative z-10 inline-flex items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-indigo-600 focus:z-20"
-                    >
-                      1
-                    </a>
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
-                    >
-                      2
-                    </a>
-                    <a
-                      href="#"
-                      className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex"
-                    >
-                      3
-                    </a>
-                    <span className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
-                      ...
-                    </span>
-                    <a
-                      href="#"
-                      className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex"
-                    >
-                      8
-                    </a>
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
-                    >
-                      9
-                    </a>
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
-                    >
-                      10
-                    </a>
-                    <a
-                      href="#"
-                      className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
-                    >
+                    </button>
+                    {pageNumber.map((p) => {
+                      console.log(p, "ppppp");
+
+                      return (
+                        <button
+                          aria-current="page"
+                          className="relative z-10 inline-flex items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-indigo-600 focus:z-20"
+                        >
+                          {p}
+                        </button>
+                      );
+                    })}
+
+                    <button className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20">
                       <span className="sr-only">Next</span>
                       <ChevronRightIcon
                         className="h-5 w-5"
                         aria-hidden="true"
                       />
-                    </a>
+                    </button>
                   </nav>
                 </div>
               </div>
