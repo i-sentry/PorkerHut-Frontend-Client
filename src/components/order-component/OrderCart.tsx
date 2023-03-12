b164a354b0f311f510126c26ee6b5ea30d24f2cd
Emmanuel Nwakanma <emmynetro@gmail.com>
2023-03-12 13:18:30 -0400

    feature/billing_page

--- src/components/order-component/OrderCart.tsx
+++ src/components/order-component/OrderCart.tsx
@@ -1,54 +1,61 @@
 import React from "react";
-import RawPorkImg from "../../assets/images/RawPorkImg.png";
+import { useSelector } from "react-redux";
+import { IProduct } from "../../redux/features/product/productSlice";
+import { RootState } from "../../redux/store";
 
 const OrderCart = () => {
+  const cart = useSelector((state: RootState) => state.product.cart)
+  const dFee = 700
+  const cartTotal = Object.values(cart).reduce((acc, current) => {
+    return acc + (parseFloat(current.price) * (current.quantity as number))
+  }, 0)
+  const vat = cartTotal + (cartTotal / 100 * 7.5)
+
   return (
-    <div className="w-[400px] bg-white rounded-lg self-start">
+    <div className="min-w-[400px] w-full lg:w-auto bg-white rounded-lg self-start lg:sticky lg:top-[100px]">
       <div className="p-4 pb-0">
         <h1 className="text-[20px] text-[#333333] font-semibold">Orders</h1>
       </div>
 
-      <OrderCard />
-      <OrderCard />
-      <OrderCard />
+      {Object.values(cart).map((item, idx) => <OrderCard item={item} key={idx} />)}
 
       <div className="py-2">
         <div className="flex justify-between px-4 py-2 font-medium">
           <span>Subtotal</span>
-          <span>₦6,000</span>
+          <span>₦{cartTotal.toLocaleString()}</span>
         </div>
         <div className="flex justify-between px-4 py-2 font-medium">
           <span>VAT</span>
-          <span>₦1,000</span>
+          <span>₦{vat.toLocaleString()}</span>
         </div>
         <div className="flex justify-between px-4 py-2 font-medium border-b border-[#D9D9D9]">
           <span>Delivery.</span>
-          <span>₦1,000</span>
+          <span>₦{dFee.toLocaleString()}</span>
         </div>
         <div className="flex justify-between px-4 py-2 font-medium">
           <span>Total</span>
-          <span>₦8,000</span>
+          <span>₦{(cartTotal + vat + dFee)?.toLocaleString()}</span>
         </div>
       </div>
     </div>
   );
 };
 
-const OrderCard = ({ item }: any) => {
+const OrderCard = ({ item }: { item: IProduct }) => {
   return (
     <div className=" flex gap-4 px-4 py-6 border-b">
       <figure className="w-[102px] overflow-hidden rounded">
-        <img src={RawPorkImg} alt="" className="w-full" />
+        <img src={item.img} alt="" className="w-full" />
       </figure>
 
       <div className="flex flex-col">
         <h1 className="text-[16px] font-semibold text-[#333333] w-40">
-          100% Healthy Feed pork Lap
+          {item.product?.name}
         </h1>
-        <h1 className=" text-[#797979] text-base">3kg x2</h1>
+        <h1 className=" text-[#797979] text-base">{item.product?.weight} x{item.quantity}</h1>
       </div>
 
-      <h1 className=" self-end md:self-start">₦3,000</h1>
+      <h1 className=" self-end md:self-start ml-auto">₦{item.price}</h1>
     </div>
   );
 };
--- src/pages/BillingPage.tsx
+++ src/pages/BillingPage.tsx
@@ -7,7 +7,7 @@ import OrderCart from "../components/order-component/OrderCart";
 const BillingPage = () => {
   return (
     <>
-      <div className="  bg-[#F5F5F5]">
+      <div className="  bg-[#F5F5F5] min-h-screen">
         <nav className="mb-20">
           <NavBar />
         </nav>
@@ -30,7 +30,7 @@ const BillingPage = () => {
             </div>
           </div>
         </div>
-        <div className="p-8 md:hidden">
+        {/* <div className="p-8 md:hidden">
           <div className=" h-[520px] w-[430px] bg-white rounded-md">
             <h1 className="p-4 text-lg font-semibold text-[#333333] ">
               Personal Information
@@ -47,7 +47,7 @@ const BillingPage = () => {
                 className=" w-96 h-16 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
               />
             </div>
-            <div className=" ml-4 my-4">
+            <div className=" my-4">
               <label className="text-[#333333] text-[14px] my-2" htmlFor="">Last Name</label>
               <input
                 type="text"
@@ -57,7 +57,7 @@ const BillingPage = () => {
                 className=" w-96 h-16 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
               />
             </div>
-            <div className=" ml-4 my-4">
+            <div className=" my-4">
               <label className=" text-[#333333] text-[14px] my-2" htmlFor="">Email Address</label>
               <input
                 type="text"
@@ -67,7 +67,7 @@ const BillingPage = () => {
                 className=" w-96 h-16 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
               />
             </div>
-            <div className=" ml-4 my-4">
+            <div className=" my-4">
               <label className=" text-[#333333] text-[14px] my-2" htmlFor="">Phone Number</label>
               <input
                 type="text"
@@ -94,7 +94,7 @@ const BillingPage = () => {
                 className=" w-96 h-16 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
               />
             </div>
-            <div className=" ml-4 my-4">
+            <div className=" my-4">
             <label className=" text-[#333333] text-[14px] my-2" htmlFor="">State</label>
               <input
                 type="text"
@@ -104,7 +104,7 @@ const BillingPage = () => {
                 className=" w-96 h-16 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
               />
             </div>
-            <div className=" ml-4 my-4">
+            <div className=" my-4">
             <label className=" text-[#333333] text-[14px] my-2" htmlFor="">Enter Your City/Town/Street</label>
               <input
                 type="text"
@@ -115,8 +115,8 @@ const BillingPage = () => {
               />
             </div>
           </div>
-        </div>
-        <div className=" text-center md:hidden">
+        </div> */}
+        {/* <div className=" text-center md:hidden">
           <div>
             <button className=" border border-[#479559] md:text-[14px] text-[16px] md:py-3 md:px-6 py-4 px-[45px] rounded-[4px] text-[#fff] bg-[#197B30] md:inline-block select-none tracking-wider font-medium whitespace-nowrap">
               Proceed to Payments
@@ -127,91 +127,128 @@ const BillingPage = () => {
               Continue to Shopping
             </button>
           </div>
-        </div>
-        <div className=" p-8 flex gap-10">
-          <div className="w-[1050px] h-[730px]  bg-white rounded-md">
-            <h1 className=" p-6 text-[20px] text-[#333333] font-semibold">
-              Billing Information
-            </h1>
-            <div className="ml-4 flex gap-10">
-              <div>
-              <label className=" text-[#333333] text-[14px] my-2" htmlFor="">First Name</label>
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  placeholder="Enter Your First Name"
-                  className=" w-[480px] h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
-                />
-              </div>
-
-              <div className="">
-              <label className=" text-[#333333] text-[14px] my-2" htmlFor="">Last Name</label>
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  placeholder="Enter Your Last Name"
-                  className=" w-[480px] h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
-                />
-              </div>
-            </div>
-            <div className=" ml-4 input my-6">
-            <label className=" text-[#333333] text-[14px] my-2" htmlFor="">Email Address</label>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Enter Your First Email Address"
-                className=" w-[1000px] h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
-              />
-            </div>
-            <div className=" ml-4 input">
-            <label className=" text-[#333333] text-[14px] my-2" htmlFor="">Phone Number</label>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="+234-555-666-6669"
-                className=" w-[1000px] h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
-              />
-            </div>
-            <div className=" ml-4 input my-6">
-            <label className=" text-[#333333] text-[14px] my-2" htmlFor="">Email Address</label>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Enter Delivery Address"
-                className=" w-[1000px] h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
-              />
-            </div>
-            <div className=" ml-4 input">
-              <h1 className=" text-[#333333] text-[14px] my-2">State</h1>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Enter State"
-                className=" w-[1000px] h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
-              />
-            </div>
-            <div className=" ml-4 input my-6">
-              <h1 className=" text-[#333333] text-[14px] my-2">
-                City/Town/State
+        </div> */}
+        <div className=" p-8 flex flex-col lg:flex-row gap-10 ">
+          <div className="billing-form flex flex-col gap-6 lg:gap-0 flex-1">
+            <div className=" bg-white rounded-md lg:rounded-b-none p-4 flex-1">
+              <h1 className=" p-6 text-[20px] text-[#333333] font-semibold">
+                Billing Information
               </h1>
-              <input
-                type="text"
-                name=""
-                id=""
-                placeholder="Enter City/Town/Street"
-                className=" w-[1000px] h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
-              />
+              <div className="flex flex-wrap gap-10">
+                <div className=" w-full">
+                  <label
+                    className=" text-[#333333] text-[14px] my-2 block"
+                    htmlFor=""
+                  >
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Enter Your First Name"
+                    className=" h-12 w-full border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
+                  />
+                </div>
+
+                <div className="w-full">
+                  <label
+                    className=" text-[#333333] text-[14px] my-2 block"
+                    htmlFor=""
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Enter Your Last Name"
+                    className=" h-12 w-full border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
+                  />
+                </div>
+              </div>
+              <div className=" input my-6">
+                <label className=" text-[#333333] text-[14px] my-2" htmlFor="">
+                  Email Address
+                </label>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Enter Your First Email Address"
+                  className=" w-full h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
+                />
+              </div>
+              <div className=" input">
+                <label className=" text-[#333333] text-[14px] my-2" htmlFor="">
+                  Phone Number
+                </label>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="+234-555-666-6669"
+                  className=" w-full h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
+                />
+              </div>
             </div>
+            <div className=" bg-white rounded-md lg:rounded-t-none p-4 flex-1">
+              <h1 className=" p-6 text-[20px] text-[#333333] font-semibold lg:hidden">
+                Delivery Information
+              </h1>
+              <div className=" input my-6">
+                <label className=" text-[#333333] text-[14px] my-2" htmlFor="">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Enter Delivery Address"
+                  className=" w-full h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
+                />
+              </div>
+              <div className=" input">
+                <h1 className=" text-[#333333] text-[14px] my-2">State</h1>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Enter State"
+                  className=" w-full h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
+                />
+              </div>
+              <div className=" input my-6">
+                <h1 className=" text-[#333333] text-[14px] my-2">
+                  City/Town/State
+                </h1>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  placeholder="Enter City/Town/Street"
+                  className=" w-full h-12 border border-[#D9D9D9] rounded-md text-[#A2A2A2] pl-5"
+                />
+              </div>
+            </div>
+            <div className=" text-center lg:bg-white rounded-md lg:rounded-t-none p-4 flex flex-col gap-3 lg:justify-end lg:flex-row flex-1 pb-4">
+              
+              <div className="">
+                <button className=" border border-[#479559] md:text-[14px] text-[16px] md:py-3 md:px-6 py-4 px-[45px] rounded-[4px] text-[#197B30] bg-[#fff] md:inline-block select-none tracking-wider font-medium whitespace-nowrap">
+                  Continue to Shopping
+                </button>
+              </div>
+              <div>
+                <button className=" border border-[#479559] md:text-[14px] text-[16px] md:py-3 md:px-6 py-4 px-[45px] rounded-[4px] text-[#fff] bg-[#197B30] md:inline-block select-none tracking-wider font-medium whitespace-nowrap">
+                  Proceed to Payments
+                </button>
+              </div>
+            </div>
+
           </div>
-          
-            <OrderCart />
-          
+
+          <OrderCart />
+
           {/* <p className=" text-sm text-[#797979] mt-2">
             Our shipping fees are flat rates. Regardless of the size and amount
             of items ordered, only one shipping fee applies.
