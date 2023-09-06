6865b561e36bb0dd6d84274b6e696de6f4571418
passionzjane <mrumunjanet@gmail.com>
2023-09-06 15:24:49 +0100

    Signup and login removed for logged in user, and logged in user displayed

--- src/components/nav-component/Aside.tsx
+++ src/components/nav-component/Aside.tsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSidebarState } from "../../store/overlay";
 import Submenu from "../../components/admin-dashboard-components/Submenu";
 import { MainSideBarLinks } from "../../utils/Navigation";
 import NavButton from "./NavButton";
 import { MdOutlinePerson } from "react-icons/md";
 
+interface User {
+  firstName: string;
+}
+
+
 const Aside = () => {
     const sideBarOpen = useSidebarState((state) => state.sideBarOpen);
+    const [user, setUser] = useState<User | null>(null);
+    // const [user, setUser] = useState(null);
+
+    useEffect(() => {
+      //@ts-ignore
+      const storedUser = JSON.parse(localStorage.getItem("user"));
+      if (storedUser !== null) {
+        setUser(storedUser);
+      } else {
+        setUser(null);
+      }
+    }, []);
+
   return (
     <div
       className={`lg:flex fixed lg:relative md:top-16 lg:top-0 left-0 z-40 bg-[#EEEEEE] lg:bg-white  w-[284px] border-r   h-screen flex-col  pt-20 transition-all duration-500 overflow-y-hidden ${
         !sideBarOpen ? "-translate-x-full lg:translate-x-0" : ""
       }`}
     >
+
+      <div className="flex items-center pl-5 my-7 gap-1">
+      <MdOutlinePerson size={24} color={"#197b30"} />
+      <span className="pl-1 text-[#333333] text-[18px] leading-[21px] font-normal whitespace-nowrap">
+       Hi, {user?.firstName || 'Guest'}
+      </span>
+  </div>
+
       {/* <div className="flex items-center pl-5 my-7 gap-1">
         <MdOutlinePerson size={24} color={"#197b30"} />
         <span className="pl-1 text-[#333333] text-[18px] leading-[21px] font-normal whitespace-nowrap">
-
+          
           Hi, {user?.firstName}
         </span>
       </div> */}
+
       {MainSideBarLinks.map((item, index) => {
         //@ts-ignore
         return (
@@ -28,10 +55,13 @@ const Aside = () => {
           </>
         );
       })}
-<div className="flex flex-col lg:hidden gap-3 pr-4 px-5 mt-4">
-  <NavButton className={signUpBtn} text="Sign Up" path="/sign-up" />
-  <NavButton className={loginBtn} text="Login" path="/login" />
-</div>
+      {
+        user === null &&  <div className="flex flex-col lg:hidden gap-3 pr-4 px-5 mt-4">
+        <NavButton className={signUpBtn} text="Sign Up" path="/sign-up" />
+        <NavButton className={loginBtn} text="Login" path="/login" />
+      </div>
+      }
+    
     </div>
   );
 };
