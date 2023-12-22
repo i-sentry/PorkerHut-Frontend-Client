837751edb4f0c1a4a0d6e82c8292b36e36a1064b
Atimscreative <timilehinsunday144@gmail.com>
2023-12-22 17:55:56 +0100

    Hero Banner card animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Services from "../components/agro-services/Services";
 import Blog from "../components/blog-component/Blog";
 import Category from "../components/category-component/Category";
@@ -9,6 +9,9 @@ import AppLayout from "../components/utility/AppLayout";
 import "aos/dist/aos.css";
 import { useAppDispatch } from "../redux/hook";
 import { fetchProduct } from "../redux/features/product/productSlice";
+import { Link } from "react-router-dom";
+//@ts-ignore
+import ScrollReveal from "scrollreveal";
 
 const Home: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -17,12 +20,41 @@ const Home: React.FC = () => {
     dispatch(fetchProduct());
   }, [dispatch]);
 
-  // useEffect(() => {
+  useEffect(() => {
+    // Initialize scroll-reveal
+    ScrollReveal().reveal(".banner", {
+      distance: "500px",
+      origin: "left",
+      duration: 1500,
+      delay: 500,
+    });
+  }, []);
 
-  // });
   return (
     <AppLayout>
-      <div className="bg-slate-400 h-[100%]">
+      <div className="relative bg-slate-400 h-[100%]">
+        <div>
+          <div className="hidden md:block banner w-full px-4 lg:pl-[80px] lg:pr-[40px] py-[40px] lg:w-7/12 absolute left-0 z-10 top-1/2 -translate-y-1/2 bg-zinc-100 bg-opacity-10 rounded backdrop-blur-3xl">
+            <h1 className="text-white text-[40px] mb-8 font-bold after:w-20 after:h-1.5 after:bg-green-700 after:absolute after:left-[80px] after:block">
+              Porker Hut Naija
+            </h1>
+            <p className="text-white text-sm font-normal leading-[150%]">
+              Porker Hut is dedicated to ethical and responsible animal rearing,
+              sourcing only the freshest, high-quality pigs from local farms.
+              Our selection of premium pork products includes succulent chops,
+              savory bacon, and mouth-watering sausages to satisfy every taste.
+              Experience the difference of our farm-to-table approach with
+              convenient online ordering and delivery options. Choose Porker Hut
+              for unparalleled quality in every bite.
+            </p>
+            <Link
+              to="/"
+              className="w-[141px] h-12 pl-[42px] pr-9 py-3 mt-6 inline-block bg-green-700 rounded text-white text-sm font-semibold"
+            >
+              Shop now
+            </Link>
+          </div>
+        </div>
         <Slider sliderImages={[]} />
       </div>
       <div className="lg:mt-24 xxs:mt-16 ">
