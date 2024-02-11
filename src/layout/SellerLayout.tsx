950481a5fff3e4d5cb972932eb41979e5175c02d
Atimscreative <timilehinsunday144@gmail.com>
2024-02-11 14:40:33 +0100

    commit

--- src/layout/SellerLayout.tsx
+++ src/layout/SellerLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import VendorsNav from "../components/vendors-component/VendorsNav";
 import SellerSideNav from "../pages/sellers-dashboard/SellerSideNav";
