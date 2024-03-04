fecf0004bf3b1b35e26bb9a60d583c486fc9a067
Atimscreative <timilehinsunday144@gmail.com>
2024-03-04 20:23:07 +0100

    Overview set

--- src/layout/AdminLayout.tsx
+++ src/layout/AdminLayout.tsx
@@ -21,7 +21,7 @@ const AdminLayout = () => {
       (!accessToken || accessToken === "undefined" || accessToken === null) &&
       !admin?.isAdmin
     ) {
-      // navigate("/admin-login");
+      navigate("/admin-login");
     }
     setLoading(false);
   }, [accessToken, admin?.isAdmin, navigate]);
--- src/pages/admin-dashboard/Overview.tsx
+++ src/pages/admin-dashboard/Overview.tsx
@@ -1,64 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AreaChart } from "./AreaChart";
-
-const data = [
-  {
-    id: "1",
-    title: "Total Sales",
-    figure: "N 400",
-  },
-  {
-    id: "2",
-    title: "Daily Revenues",
-    figure: "N400",
-  },
-  {
-    id: "3",
-    title: "Items Sold",
-    figure: "900",
-  },
-  {
-    id: "4",
-    title: "Average Order Value",
-    figure: "500",
-  },
-  {
-    id: "5",
-    title: "Total Orders",
-    figure: "3000",
-  },
-];
-const orderData = [
-  {
-    id: "1",
-    title: "Average Daily Order",
-    figure: "200",
-  },
-  {
-    id: "2",
-    title: "Pending Orders",
-    figure: "20",
-  },
-  {
-    id: "3",
-    title: "Fulfilled Orders",
-    figure: "1000",
-  },
-  {
-    id: "4",
-    title: "Failed Orders",
-    figure: "50",
-  },
-  {
-    id: "5",
-    title: "Returned Order",
-    figure: "100",
-  },
-];
+import { useGetOrders } from "../../services/hooks/orders";
 
 const Overview = () => {
+  const [orders, setOrders] = useState<any[]>([]);
+  const { data: ordersList, isLoading } = useGetOrders();
   const color = (val: {
-    
     title:
       | string
       | number
@@ -80,8 +27,8 @@ const Overview = () => {
       case "Pending Orders":
         return (
           <>
-            <h1 className="text-[#F29339] text-xs font-light">{val?.title}</h1>
-            <span className="text-xl text-[#F29339] font-medium">
+            <h1 className="text-xs font-light text-[#F29339]">{val?.title}</h1>
+            <span className="text-xl font-medium text-[#F29339]">
               {val?.figure}
             </span>
           </>
@@ -89,8 +36,8 @@ const Overview = () => {
       case "Fulfilled Orders":
         return (
           <>
-            <h1 className="text-[#22C55E] text-xs font-light">{val?.title}</h1>
-            <span className="text-xl text-[#22C55E] font-medium">
+            <h1 className="text-xs font-light text-[#22C55E]">{val?.title}</h1>
+            <span className="text-xl font-medium text-[#22C55E]">
               {val?.figure}
             </span>
           </>
@@ -98,8 +45,8 @@ const Overview = () => {
       case "Failed Orders":
         return (
           <>
-            <h1 className="text-[#F91919] text-xs font-light">{val?.title}</h1>
-            <span className="text-xl text-[#F91919] font-medium">
+            <h1 className="text-xs font-light text-[#F91919]">{val?.title}</h1>
+            <span className="text-xl font-medium text-[#F91919]">
               {val?.figure}
             </span>
           </>
@@ -108,22 +55,112 @@ const Overview = () => {
       default:
         return (
           <>
-            <h1 className="text-[#A2A2A2] text-xs font-light">{val?.title}</h1>
+            <h1 className="text-xs font-light text-[#A2A2A2]">{val?.title}</h1>
             <span className="text-xl font-medium">{val?.figure}</span>
           </>
         );
     }
   };
 
+  useEffect(() => {
+    if (!isLoading) setOrders(ordersList?.data.data);
+  }, [ordersList?.data.data, isLoading]);
+
   React.useEffect(() => {
     window.scrollTo(0, 0); // scrolls to top-left corner of the page
   }, []);
+
+  // OVERVIEW STATS
+  const totalSales = orders.reduce(
+    (acc: any, order: any, index: number) => acc + order?.subtotal,
+    0,
+  );
+
+  const dailyRevenue = orders
+    ?.filter((order: any) => {
+      const currentDate = new Date();
+      const orderDate = new Date(order?.orderDate);
+      return (
+        orderDate.getFullYear() === currentDate.getFullYear() &&
+        orderDate.getMonth() === currentDate.getMonth() &&
+        orderDate.getDate() === currentDate.getDate()
+      );
+    })
+    .reduce((acc: any, cur: any) => acc + cur?.subtotal, 0);
+
+  const itemsSold = orders
+    ?.map((order: any) => order?.productDetails.length)
+    .reduce((acc: any, cur: any) => acc + cur, 0);
+
+  const getOrderStatusLength = (orders: any[], status: string) => {
+    return orders?.filter((order) => order?.status.toLowerCase() === status)
+      ?.length;
+  };
+
+  console.log(orders);
+  console.log(itemsSold);
+
+  const data = [
+    {
+      id: "1",
+      title: "Total Sales",
+      figure: `₦${totalSales.toLocaleString()}`,
+    },
+    {
+      id: "2",
+      title: "Daily Revenues",
+      figure: `₦${dailyRevenue.toLocaleString()}`,
+    },
+    {
+      id: "3",
+      title: "Items Sold",
+      figure: `${itemsSold}`,
+    },
+    {
+      id: "4",
+      title: "Average Order Value",
+      figure: `${Math.trunc(totalSales / orders?.length).toLocaleString()}`,
+    },
+    {
+      id: "5",
+      title: "Total Orders",
+      figure: `${orders?.length}`,
+    },
+  ];
+
+  const orderData = [
+    {
+      id: "1",
+      title: "Average Daily Order",
+      figure: "200",
+    },
+    {
+      id: "2",
+      title: "Pending Orders",
+      figure: `${getOrderStatusLength(orders, "pending")}`,
+    },
+    {
+      id: "3",
+      title: "Fulfilled Orders",
+      figure: `${getOrderStatusLength(orders, "completed")}`,
+    },
+    {
+      id: "4",
+      title: "Failed Orders",
+      figure: `${getOrderStatusLength(orders, "failed")}`,
+    },
+    {
+      id: "5",
+      title: "Returned Order",
+      figure: `${getOrderStatusLength(orders, "returned")}`,
+    },
+  ];
 
   return (
     <div className="pl-10 pt-10 pr-5">
       <div className="mb-5">
         <h1 className="text-2xl font-medium ">Overview</h1>
-        <span className="text-[#A2A2A2] font-normal text-sm">
+        <span className="text-sm font-normal text-[#A2A2A2]">
           This is an overview of Porker Hut.
         </span>
       </div>
@@ -131,20 +168,20 @@ const Overview = () => {
         {data.map((val) => (
           <div
             key={val.id}
-            className="bg-[#F4F4F4] h-full flex flex-col flex-1 border-[#D9D9D9] border-r-[1px] gap-2 rounded-l-sm items-start justify-center px-6 py-3"
+            className="flex h-full flex-1 flex-col items-start justify-center gap-2 rounded-l-sm border-r-[1px] border-[#D9D9D9] bg-[#F4F4F4] px-6 py-3"
           >
-            <h1 className="text-[#A2A2A2] text-xs  font-normal">
+            <h1 className="text-xs font-normal  text-[#A2A2A2]">
               {val?.title}
             </h1>
             <span className="text-xl font-medium">{val?.figure}</span>
           </div>
         ))}
       </div>
-      <div className="flex items-center justify-items-stretch mt-4">
+      <div className="mt-4 flex items-center justify-items-stretch">
         {orderData.map((val) => (
           <div
             key={val.id}
-            className="bg-[#F4F4F4] h-full flex flex-col flex-1 border-[#D9D9D9] border-r-[1px] gap-2 rounded-l-sm items-start justify-center px-6 py-3"
+            className="flex h-full flex-1 flex-col items-start justify-center gap-2 rounded-l-sm border-r-[1px] border-[#D9D9D9] bg-[#F4F4F4] px-6 py-3"
           >
             <div>{color(val)}</div>
           </div>
