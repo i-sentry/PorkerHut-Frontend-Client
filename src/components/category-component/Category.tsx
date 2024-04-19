5f25633924877b45ea844aecb3e0aaced6c2292b
atimscreative <timilehinsunday144@gmail.com>
2024-04-20 00:49:39 +0100

    Category section skeleton loader fixed

--- src/components/category-component/Category.tsx
+++ src/components/category-component/Category.tsx
@@ -10,6 +10,7 @@ import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
 
 // Import Swiper styles
 import "swiper/css";
+import { divide } from "lodash";
 export interface ICategory {
   createdAt: string;
   description: string;
@@ -60,40 +61,52 @@ const Category = () => {
                 <Card key={index} item={item} />
               ))}
         </div> */}
-        <Swiper
-          spaceBetween={30}
-          slidesPerView={1}
-          cssMode={true}
-          // navigation={true}
-          // pagination={true}
-          breakpoints={{
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
-          }}
-          mousewheel={true}
-          keyboard={true}
-          modules={[Navigation, Mousewheel, Keyboard]}
-          className="mySwiper flex space-x-4 p-14 px-0 xxs:hidden md:flex lg:flex"
-        >
-          {isLoading
-            ? // Render skeleton loaders when loading
-              Array.from({ length: 3 }).map((_, index) => (
-                //@ts-ignore
-                <SkeletonLoader key={index} />
-              ))
-            : // Render cards when data is available
-              allCategories?.data.map((item: ICategory, index: number) => (
-                <SwiperSlide>
-                  <Card key={index} item={item} />
-                </SwiperSlide>
-              ))}
-        </Swiper>
+        {isLoading && (
+          // Render skeleton loaders when loading
+          <div className="flex w-full space-x-4">
+            {Array.from({ length: 3 }).map((_, index) => (
+              //@ts-ignore
+              <SkeletonLoader
+                key={index}
+                className={`sm:w-[calc(50%_-_10px)] md:w-[calc(33%_-_10px)] ${index === 1 && "hidden sm:block"} ${index === 2 && "hidden"} md:block `}
+              />
+            ))}
+          </div>
+        )}
+
+        {!isLoading && allCategories?.data?.length >= 1 && (
+          <Swiper
+            spaceBetween={30}
+            slidesPerView={1}
+            cssMode={true}
+            // navigation={true}
+            // pagination={true}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+              },
+            }}
+            mousewheel={true}
+            keyboard={true}
+            modules={[Navigation, Mousewheel, Keyboard]}
+            className={` mySwiper flex space-x-4 p-14 px-0 xxs:hidden md:flex lg:flex`}
+          >
+            {allCategories?.data.map((item: ICategory, index: number) => (
+              <SwiperSlide>
+                <Card key={index} item={item} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+
+        {!isLoading && allCategories?.data?.length < 1 && (
+          <div>No category available</div>
+        )}
 
         {/* <div className="container mx-auto px-4 overflow-x-scroll">
           <div className="flex gap-8 py-8">
@@ -163,10 +176,10 @@ export default Category;
 //   );
 // };
 
-export const SkeletonLoader = () => {
+export const SkeletonLoader = ({ className }: { className?: any }) => {
   return (
     // <div className="animate-pulse bg-gray-400 rounded-sm w-full h-[400px] relative "></div>
-    <div className="relative w-full overflow-hidden">
+    <div className={`relative w-full overflow-hidden ${className}`}>
       <div className="skeleton-loader"></div>
       <div className="flex w-full items-center justify-between">
         <div className="w-full">
