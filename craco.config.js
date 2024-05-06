82092054b3c311a0e42023287f98bba0aedf1f67
atimscreative <timilehinsunday144@gmail.com>
2024-05-06 21:09:08 +0100

    Configured BrowserList, Postcss autoprefixer

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require("autoprefixer"),
+        // You can add other PostCSS plugins here if needed
+      ],
+    },
+  },
+};
