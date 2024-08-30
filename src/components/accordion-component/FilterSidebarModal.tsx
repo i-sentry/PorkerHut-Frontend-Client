4be2970648fba8eb5728e60d223225b6ff7fb768
atimscreative <timilehinsunday144@gmail.com>
2024-08-30 05:17:14 +0100

    SCRUM 7 Fixed

--- src/components/accordion-component/FilterSidebarModal.tsx
+++ src/components/accordion-component/FilterSidebarModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Filtercomp from "../custom-filter/FilterComp";
 import { BsX } from "react-icons/bs";
+import { cn } from "../../helper/cn";
 
 interface FilterSidebarProps {
   open: boolean;
@@ -27,10 +28,14 @@ const FilterSidebar = ({
   setData,
   handleClick,
 }: FilterSidebarProps) => {
-  if (!open) return null;
   return (
     <>
-      <div className="fixed left-0 z-50 h-full w-[80%] overflow-hidden bg-white py-20 px-4 pb-[120px] xxs:block lg:hidden">
+      <div
+        className={cn(
+          "fixed z-[60] h-full w-[90%] max-w-md overflow-hidden bg-white py-20 px-4 pt-14 pb-[120px] duration-300 xxs:block lg:hidden",
+          open ? "left-0" : "-left-full",
+        )}
+      >
         <div className=" flex items-center justify-between">
           <span className=" text-[16px] font-medium ">Filters</span>
           <span
@@ -41,46 +46,46 @@ const FilterSidebar = ({
             close
           </span>
         </div>
-        <div className="h-full">
-          {/* <Filter
-            setData={setData}
-            // menuItem={menuItem}
-            handleClick={handleClick}
-            // 
+        <div className="flex h-full flex-col justify-between">
+          <div className="hide-scroll-bar max-h-[500px] overflow-y-auto">
+            <div className="h-max">
+              <Filtercomp
+                selectedItems={selectedItems}
+                setSelectedItems={setSelectedItems}
+                data={data}
+                handleApplyClick={handleApplyClick}
+                handleClear={handleClear}
+              />
+            </div>
+          </div>
 
-            
-          /> */}
-
-          <Filtercomp
-            selectedItems={selectedItems}
-            setSelectedItems={setSelectedItems}
-            data={data}
-            handleApplyClick={handleApplyClick}
-            handleClear={handleClear}
-          />
-        </div>
-
-        <div className="flex items-center justify-center gap-3">
-          <button
-            onClick={() => {
-              handleClear();
-              onClose();
-            }}
-            className="rounded border border-red-500 py-3 px-6 text-red-500 shadow-md"
-          >
-            Clear filter
-          </button>
-          <button
-            onClick={handleApplyClick}
-            className="rounded bg-[#197B30] py-3 px-6 text-white shadow-md"
-          >
-            Show result
-          </button>
+          <div className="flex items-center justify-center gap-3">
+            <button
+              onClick={() => {
+                handleClear();
+                onClose();
+              }}
+              className="rounded border border-red-500 py-3 px-6 text-sm text-red-500 shadow-md"
+            >
+              Clear filter
+            </button>
+            <button
+              onClick={handleApplyClick}
+              className="rounded bg-[#197B30] py-3 px-6 text-sm text-white shadow-md"
+            >
+              Show result
+            </button>
+          </div>
         </div>
       </div>
       <div
         onClick={onClose}
-        className="absolute top-0  right-0 z-50 min-h-screen w-1/5 bg-gray-800 bg-opacity-50 xxs:fixed lg:hidden"
+        className={cn(
+          "absolute top-0 left-0 z-50 min-h-screen w-full bg-gray-800 bg-opacity-50 duration-300 xxs:fixed lg:hidden",
+          open
+            ? "pointer-events-auto opacity-100"
+            : "pointer-events-none opacity-0",
+        )}
       ></div>
     </>
   );
